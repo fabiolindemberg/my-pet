e9d09641c8bc8cf3f70c1849b2dc2fd26d07d21f
fabiolindemberg <fabiolindemberg@gmail.com>
2018-12-14 20:55:41 -0300

    Adicionando evento de click aos TextViews

--- app/src/main/java/mypet/fabiolindemberg/com/br/mypet/MainActivity.java
+++ app/src/main/java/mypet/fabiolindemberg/com/br/mypet/MainActivity.java
@@ -2,6 +2,9 @@ package mypet.fabiolindemberg.com.br.mypet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,31 @@ public class MainActivity extends AppCompatActivity {
         Pets.add(new Pet(1, "Pastor Alemão", true, "Pastor alemão sem pedigri, com dois meses muito dócil"));
         Pets.add(new Pet(2, "Gato Angorá", true, "Gatinho fofo"));
         Pets.add(new Pet(3, "Periquito", true, "Doo o passáro e a gaiola!"));
+
+        TextView tvCat = findViewById(R.id.tvCat);
+        TextView tvDog = findViewById(R.id.tvDog);
+        TextView tvOthers = findViewById(R.id.tvOthers);
+
+        tvCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Filtro", "Gato");
+            }
+        });
+
+        tvDog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Filtro", "Cão");
+            }
+        });
+
+        tvOthers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Filtro", "Outros");
+            }
+        });
 
     }
 }
